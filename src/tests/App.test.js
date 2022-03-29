9f4e60c68b325081dab5edd18632bd06de3ec59a
Matheus S Alves <matheus.souza.alves@gmail.com>
2022-03-29 13:31:18 -0300

    requisito 1 :heavy_check_mark:

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,62 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe(
+  'testa componente App', () => {
+    it('testa se aplicação contém um conjunto fixo de links de navegação', () => {
+      renderWithRouter(<App />);
+      const navBar = screen.getByRole('navigation');
+      const nOfLinks = 3;
+      const homeLink = screen.getByRole('link', { name: /home/i });
+      const aboutLink = screen.getByRole('link', { name: /about/i });
+      const favoriteLink = screen.getByRole('link', { name: /favorite pokémons/i });
+
+      expect(navBar.children.length).toBe(nOfLinks);
+      expect(homeLink).toBeDefined();
+      expect(aboutLink).toBeDefined();
+      expect(favoriteLink).toBeDefined();
+    });
+
+    it('testa se é redirecionado para a URL certa ao clicar no link Home', () => {
+      const { history } = renderWithRouter(<App />);
+      const homeLink = screen.getByRole('link', { name: /home/i });
+
+      userEvent.click(homeLink);
+
+      const { pathname } = history.location;
+      expect(pathname).toBe('/');
+    });
+
+    it('testa se é redirecionado para a URL certa ao clicar no link About', () => {
+      const { history } = renderWithRouter(<App />);
+      const homeLink = screen.getByRole('link', { name: /about/i });
+
+      userEvent.click(homeLink);
+
+      const { pathname } = history.location;
+      expect(pathname).toBe('/about');
+    });
+
+    it('testa se é redirecionado para a URL certa ao clicar no link Favorite', () => {
+      const { history } = renderWithRouter(<App />);
+      const homeLink = screen.getByRole('link', { name: /favorite pokémons/i });
+
+      userEvent.click(homeLink);
+
+      const { pathname } = history.location;
+      expect(pathname).toBe('/favorites');
+    });
+
+    it('testa se é redirecionado para not found ao entrar em URL desconhecida', () => {
+      const { history } = renderWithRouter(<App />);
+      history.push('/url/fake');
+      const notFoundMessage = screen.getByRole('heading',
+        { level: 2, name: /Page requested not found/i });
+
+      expect(notFoundMessage).toBeDefined();
+    });
+  },
+);
