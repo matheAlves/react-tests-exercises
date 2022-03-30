8805f9d3a3676cafa77e4657c6b7e5919ed9e855
Matheus S Alves <matheus.souza.alves@gmail.com>
2022-03-29 21:47:16 -0300

    requisito 3 :heavy_check_mark:

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from './renderWithRouter';
+import pokemons from '../data';
+
+describe(
+  'testa componente FavoritePokemons', () => {
+    it('testa se é exibida mensagem correta se não houver favoritos', () => {
+      renderWithRouter(<FavoritePokemons />);
+      const message = screen.getByText(/No favorite pokemon found/i);
+      expect(message).toBeDefined();
+    });
+
+    it('testa se é exibido todos os cards de  favoritos', () => {
+      const length = 9;
+      renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+      const favPokemon = screen.getAllByTestId('pokemon-name');
+      expect(favPokemon).toHaveLength(length);
+    });
+  },
+);
