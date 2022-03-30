df8b401b59de582364dad495e418f2a5d8d051f3
Matheus S Alves <matheus.souza.alves@gmail.com>
2022-03-29 23:33:35 -0300

    requisito 4 :heavy_check_mark:

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../components';
+import renderWithRouter from './renderWithRouter';
+
+describe(
+  'testa componente NotFound', () => {
+    it('testa se a página contem um h2 com o texto correto', () => {
+      renderWithRouter(<NotFound />);
+      const message = screen.getByRole('heading',
+        { level: 2, name: /Page requested not found/i });
+      expect(message).toBeDefined();
+    });
+
+    it('testa se exibe a imagem correta', () => {
+      renderWithRouter(<NotFound />);
+      const image = screen.getByAltText(
+        'Pikachu crying because the page requested was not found',
+      );
+      expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    });
+  },
+);
